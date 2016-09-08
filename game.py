f73c2f5acb11745bc3b9bd75fcf4989c25299c9b
fireclawthefox <fireclawthefox@gmail.com>
2016-09-09 01:21:42 +0200

    added THE LIGHTSHOW

--- game.py
+++ game.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
 
+from random import choice
+
 # Panda Engine imports
-from panda3d.core import CardMaker, NodePath, DirectionalLight
+from panda3d.core import (
+    CardMaker,
+    NodePath,
+    DirectionalLight,
+    PointLight,
+    VBase4)
 from direct.task.Task import Task
 
 # Game imports
@@ -43,6 +50,9 @@ class Game():
         # HUD
         self.hud = Hud()
 
+        # Lightshow
+        self.elapsed = 0.0
+
     def start(self):
         self.loadLevel("assets/level")
         self.loadLights()
@@ -56,13 +66,17 @@ class Game():
 
         self.hud.show()
 
+        # start the Lightshow
+        taskMgr.add(self.discoLightTask, "the lightshow")
+
     def stop(self):
         #self.player.stop()
         #self.dude.stop()
         #self.physics_world = None
-        render.clearLight(self.directLight)
+        render.clearLight()
         self.directLight = None
         self.hud.hide()
+        taskMgr.remove("the lightshow")
 
     def update(self, task):
 
@@ -111,6 +125,73 @@ class Game():
         dlnp.setHpr(-30, 0, 0)
         render.setLight(dlnp)
         self.directLight = dlnp
+
+        self.discoLights = []
+
+        p1 = PointLight("PointLight1")
+        p1.setColor(VBase4(1, 0, 0, 1))
+        p1.setAttenuation((0.08, 0, 0.05))
+        p1np = render.attachNewNode(p1)
+        p1np.setPos(0, -5, 0)
+        render.setLight(p1np)
+        self.discoLights.append(p1)
+
+        p2 = PointLight("PointLight2")
+        p2.setColor(VBase4(0, 1, 0, 1))
+        p2.setAttenuation((0.08, 0, 0.05))
+        p2np = render.attachNewNode(p2)
+        p2np.setPos(5, -5, 0)
+        render.setLight(p2np)
+        self.discoLights.append(p2)
+
+        p3 = PointLight("PointLight3")
+        p3.setColor(VBase4(0, 0, 1, 1))
+        p3.setAttenuation((0.08, 0, 0.05))
+        p3np = render.attachNewNode(p3)
+        p3np.setPos(-5, -5, 0)
+        render.setLight(p3np)
+        self.discoLights.append(p3)
+
+        p4 = PointLight("PointLight4")
+        p4.setColor(VBase4(0, 0, 1, 1))
+        p4.setAttenuation((0.08, 0, 0.05))
+        p4np = render.attachNewNode(p4)
+        p4np.setPos(-5, -5, 5)
+        render.setLight(p4np)
+        self.discoLights.append(p4)
+
+        p5 = PointLight("PointLight1")
+        p5.setColor(VBase4(0, 0, 1, 1))
+        p5.setAttenuation((0.08, 0, 0.05))
+        p5np = render.attachNewNode(p5)
+        p5np.setPos(0, -5, 5)
+        render.setLight(p5np)
+        self.discoLights.append(p5)
+
+        p6 = PointLight("PointLight1")
+        p6.setColor(VBase4(0, 0, 1, 1))
+        p6.setAttenuation((0.08, 0, 0.05))
+        p6np = render.attachNewNode(p6)
+        p6np.setPos(5, -5, 5)
+        render.setLight(p6np)
+        self.discoLights.append(p6)
+
+    def discoLightTask(self, task):
+        self.elapsed += globalClock.getDt()
+
+        if self.elapsed > 0.75:
+            for light in self.discoLights:
+                newcolor = choice(
+                    [VBase4(0, 0, 1, 1),
+                    VBase4(0, 1, 0, 1),
+                    VBase4(1, 0, 0, 1),
+                    VBase4(0, 1, 1, 1),
+                    VBase4(1, 0, 1, 1),
+                    VBase4(1, 1, 0, 1),]
+                    )
+                light.setColor(newcolor)
+            self.elapsed = 0.0
+        return task.cont
 
     def loadPlayer(self, _name):
         self.player = Player(self)
