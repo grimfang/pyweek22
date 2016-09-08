6cc6b2f06059659dff78cfa4d2f364608e29d0e1
fireclawthefox <fireclawthefox@gmail.com>
2016-09-09 01:19:29 +0200

    Added more control keys and soundeffects for the doors

--- player.py
+++ player.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+from random import choice
+
 # Panda Engine imports
 from panda3d.bullet import BulletBoxShape, BulletRigidBodyNode, BulletHingeConstraint
 from panda3d.core import Vec3, BitMask32, Point3
@@ -14,6 +16,9 @@ class Player():
 
     def __init__(self, _parent=None):
         self.parent = _parent
+        self.doorsfx = [
+            loader.loadSfx("sfx/flipperSlam1.ogg"),
+            loader.loadSfx("sfx/flipperSlam2.ogg")]
 
 
     def start(self):
@@ -23,12 +28,22 @@ class Player():
     	base.accept("arrow_right", self.rightAction)
     	base.accept("z", self.leftAction)
     	base.accept("/", self.rightAction)
+        base.accept("lcontrol", self.leftAction)
+    	base.accept("rcontrol", self.rightAction)
+        base.accept("lalt", self.leftAction)
+    	base.accept("ralt", self.rightAction)
+        base.accept("lshift", self.leftAction)
+    	base.accept("rshift", self.rightAction)
 
     def stop(self):
     	base.ignore("arrow_left")
     	base.ignore("arrow_right")
     	base.ignore("z")
     	base.ignore("/")
+        base.ignore("lcontrol")
+    	base.ignore("rcontrol")
+        base.ignore("lalt")
+    	base.ignore("ralt")
     	taskMgr.remove('Player_Update_Task')
     	self.parent.game_doors = {}
 
@@ -54,8 +69,11 @@ class Player():
     def leftAction(self):
     	#self.parent.game_doors['left'].node().applyTorqueImpulse(Vec3(0, -100, 0))
     	self.parent.game_doors['left_hinge'].enableAngularMotor(True, 20.0, 15.0)
+        choice(self.doorsfx).play()
+
 
     def rightAction(self):
     	#self.parent.game_doors['right'].node().applyTorqueImpulse(Vec3(0, 100, 0))
     	#ulimit = self.parent.game_doors['right_hinge'].getUpperLimit()
     	self.parent.game_doors['right_hinge'].enableAngularMotor(True, 20.0, 15.0)
+        choice(self.doorsfx).play()
