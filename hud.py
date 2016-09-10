437f058323898ad370527480c65e97f8d563aebc
fireclawthefox <fireclawthefox@gmail.com>
2016-09-10 03:53:26 +0200

    Fixed hud placement and added control notes

--- hud.py
+++ hud.py
@@ -5,24 +5,35 @@ from direct.gui.DirectGui import (
 
 class Hud():
     def __init__(self):
+        wp = base.win.getProperties()
+        aspX = 1.0
+        aspY = 1.0
+        wpXSize = wp.getXSize()
+        wpYSize = wp.getYSize()
+        aspect = wpXSize / float(wpYSize)
+
+        paneWidth = 0.4444445
+
         self.frameMain = DirectFrame(
             image="hudBackground.png",
-            image_scale=(0.4444445, 1, 1),
+            image_scale=(paneWidth, 1, 1),
+            image_pos=(paneWidth, 0, 0),
             frameSize=(
-                base.a2dLeft/4.0, base.a2dRight/4.0,
+                0, base.a2dRight/2.0,
                 base.a2dBottom, base.a2dTop),
             frameColor=(0, 0, 0, 0),
-            pos=(base.a2dLeft/1.33333, 0, 0))
+            pos=(base.a2dLeft, 0, 0))
         self.frameMain.setTransparency(True)
 
         self.frameRightPane = DirectFrame(
             image="hudBackgroundRight.png",
-            image_scale=(0.4444445, 1, 1),
+            image_scale=(paneWidth, 1, 1),
+            image_pos=(-paneWidth, 0, 0),
             frameSize=(
-                base.a2dRight/4.0, base.a2dLeft/4.0,
+                base.a2dLeft/2.0, 0,
                 base.a2dBottom, base.a2dTop),
             frameColor=(0, 0, 0, 0),
-            pos=(base.a2dRight/1.33333, 0, 0))
+            pos=(base.a2dRight, 0, 0))
         self.frameRightPane.setTransparency(True)
         self.hide()
 
@@ -30,7 +41,7 @@ class Hud():
             text="Scoreboard",
             text_fg=(240/255.0,255/255.0,240/255.0,1),
             scale=0.1,
-            pos=(0,0,0.8),
+            pos=(paneWidth,0,0.8),
             frameColor=(0,0,0,0),)
         self.lblScore.setTransparency(True)
         self.lblScore.reparentTo(self.frameMain)
@@ -39,8 +50,8 @@ class Hud():
             text="Bad dudes: 0",
             text_fg=(240/255.0,255/255.0,240/255.0,1),
             text_align=TextNode.ALeft,
-            scale=0.1,
-            pos=(-0.4,0,0.5),
+            scale=0.08,
+            pos=(0.1,0,0.5),
             frameColor=(0,0,0,0),)
         self.lblReds.setTransparency(True)
         self.lblReds.reparentTo(self.frameMain)
@@ -49,12 +60,41 @@ class Hud():
             text="Good dudes: 0",
             text_fg=(240/255.0,255/255.0,240/255.0,1),
             text_align=TextNode.ALeft,
-            scale=0.1,
-            pos=(-0.4,0,0.3),
+            scale=0.08,
+            pos=(0.1,0,0.3),
             frameColor=(0,0,0,0),)
         self.lblReds.setTransparency(True)
         self.lblBlues.reparentTo(self.frameMain)
         self.hide()
+
+        self.lblInfo = DirectLabel(
+            text="Controlls",
+            text_fg=(240/255.0,255/255.0,240/255.0,1),
+            scale=0.1,
+            pos=(-paneWidth,0,0.8),
+            frameColor=(0,0,0,0),)
+        self.lblInfo.setTransparency(True)
+        self.lblInfo.reparentTo(self.frameRightPane)
+
+        self.lblRight = DirectLabel(
+            text="~ Right Door ~\nRight Arrow\nRight Alt\nRight Ctrl\n/",
+            text_fg=(240/255.0,255/255.0,240/255.0,1),
+            scale=0.05,
+            pos=(-paneWidth,0,0.7),
+            frameColor=(0,0,0,0),
+            sortOrder = 0)
+        self.lblRight.setTransparency(True)
+        self.lblRight.reparentTo(self.frameRightPane)
+
+        self.lblLeft = DirectLabel(
+            text="~ Left Door ~\nLeft Arrow\nLeft Alt\nLeft Ctrl\nZ",
+            text_fg=(240/255.0,255/255.0,240/255.0,1),
+            scale=0.05,
+            pos=(-paneWidth,0,0.4),
+            frameColor=(0,0,0,0),)
+        self.lblLeft.setTransparency(True)
+        self.lblLeft.reparentTo(self.frameRightPane)
+
 
     def show(self):
         self.frameMain.show()
